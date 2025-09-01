f3ac67ba4e747bdb5a3a87dc481a78bcd92cce80
Lucas F Vilela <lucasvilelapersonal@gmail.com>
2025-08-31 21:00:21 -0300

    feat: inicial setup

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css';
+
+function Banner() {
+    return (
+        <header className='banner'>
+            <img src="/imagens/banner.png" alt="O banner do organo"></img>
+        </header>
+)
+    
+}
+
+export default Banner;
