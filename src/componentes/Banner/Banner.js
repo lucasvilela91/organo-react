ef23f590b47bc088394c99e96f08976cad651742
Lucas Freitas Vilela <152106763+lucasvilela91@users.noreply.github.com>
2025-09-02 23:04:19 -0300

    feat: Add collaborator form

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,6 +1,6 @@
 import './Banner.css';
 
-function Banner() {
+export const Banner = () => {
     return (
         <header className='banner'>
             <img src="/imagens/banner.png" alt="O banner do organo"></img>
@@ -9,4 +9,3 @@ function Banner() {
     
 }
 
-export default Banner;
