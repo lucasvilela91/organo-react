4f85680ab436e18e9abedb54f33fcca9e0350bf1
Lucas F Vilela <lucasvilelapersonal@gmail.com>
2025-09-07 15:37:34 -0300

    feat: Add button component and require form fields

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,9 +1,9 @@
+import Botao from '../Botao';
 import CampoTexto from '../CampoTexto';
 import ListaSuspensa from '../ListaSuspensa';
 import './Formulario.css';
 
 const Formulario = () => {
-
   const times = [
     'Programação',
     'Front-end',
@@ -13,16 +13,23 @@ const Formulario = () => {
     'UX e Design',
     'Mobile',
     'Inovação e Gestão',
-    'IA'
-  ]
+    'IA',
+  ];
+
+  const aoSalvar = (evento) => {
+    evento.preventDefault()
+    console.log('Form foi submetido!')
+  }
+
   return (
-    <section className='formulario'>
-      <form>
+    <section className="formulario">
+      <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do colaborador:</h2>
-        <CampoTexto label='Nome' placeholder='Digite seu nome' />
-        <CampoTexto label='Cargo' placeholder='Digite seu cargo' />
-        <CampoTexto label='Imagem' placeholder='Digite o endereço da imagem' />
-        <ListaSuspensa label='Time' itens={times} />
+        <CampoTexto obrigatorio='true' label="Nome" placeholder="Digite seu nome" />
+        <CampoTexto obrigatorio='true' label="Cargo" placeholder="Digite seu cargo" />
+        <CampoTexto obrigatorio='true' label="Imagem" placeholder="Digite o endereço da imagem" />
+        <ListaSuspensa label="Time" itens={times} />
+        <Botao>Criar Card </Botao>
       </form>
     </section>
   );
