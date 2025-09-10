73c645c3f5583130f368b724a53fcee76eeb77a5
Lucas Freitas Vilela <152106763+lucasvilela91@users.noreply.github.com>
2025-09-09 22:26:07 -0300

    feat: add Colaborador component and integrate into Time

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/lucasvilela91.png' alt='Avatar Lucas'></img>
+            </div>
+            <div className='rodape'>
+                <h4>Lucas Vilela</h4>
+                <h5>Estagiário</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador;
